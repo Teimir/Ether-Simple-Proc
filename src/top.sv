85f3c6e91affbf057934c594af1f0faa4039327b
Teimir <sobakin_ru@mail.ru>
2025-03-29 23:13:06 +0300

    FIX[HDL] Добавил вывод линии ввода вывода в топ

--- src/top.sv
+++ src/top.sv
@@ -19,13 +19,19 @@ ram u0(
 );
 
 core u1(
-  .clk_i(clk_i),
-  .rst_i(rst_i),
+  .clk_i         (clk_i),
+  .rst_i         (rst_i),
   // A\D line
-  .address_o(ram_address_line),  
-  .data_i(ram_data_from_r_line),
-  .we_i(ram_we),              
-  .data_o(ram_data_to_r_line)
+  .address_o     (ram_address_line),  
+  .data_i        (ram_data_from_r_line),
+  .we_i          (ram_we),              
+  .data_o        (ram_data_to_r_line),
+ // IO A\D line
+  .irq_i         (),          
+  .io_data_i     (),
+  .io_data_o     (),
+  .io_addr_o     (),
+  .io_we_o       (),      
 );
 
   
